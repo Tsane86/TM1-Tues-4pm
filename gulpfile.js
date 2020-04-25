b9c5c6b213df32ad3428e610567053b572c4c8d6
asdsadv <62586440+asdsadv@users.noreply.github.com>
2020-04-25 22:08:48 +1000

    Update to master (#1)
    * Add files via upload
    
    * Folders, Gulp.js, node.js and react.js installed
    
    * Update README.md
    
    * Update InitDB.sql
    
    * Update with Bootstrap, nav and login being built
    
    * Added login page
    
    * Login + Registration Screens
    
    * Home updated
    
    * Updated index.html
    
    * Added Consultiation
    
    * Added nav.html
    
    Co-authored-by: ademdeojee <54884928+ademdeojee@users.noreply.github.com>
    Co-authored-by: Brenton Gibbons @ Serenity <brenton@breakdesign.com.au>
    Co-authored-by: Tsane86 <44742526+Tsane86@users.noreply.github.com>
    Co-authored-by: JasonI136 <63634201+JasonI136@users.noreply.github.com>
    Co-authored-by: Daisy <daisyli14598@gmail.com>

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+const gulp = require("gulp"),
+  sass = require("gulp-sass"),
+  sourcemaps = require("gulp-sourcemaps"),
+  browserSync = require("browser-sync").create(),
+  csso = require("gulp-csso");
+(source = "./process/"), (dest = "./builds/ehealthcare/");
+
+sass.compiler = require("node-sass");
+
+function html() {
+  return gulp.src(dest + "**/*.html");
+}
+
+function js() {
+  return gulp.src(dest + "**/*.js");
+}
+
+function styles() {
+  return (
+    gulp
+      .src(source + "sass/style.scss")
+      .pipe(sourcemaps.init())
+      .pipe(
+        sass({
+          sourcemap: true,
+          style: "compressed",
+        }).on("error", sass.logError)
+      )
+      // Minify the file
+      .pipe(csso())
+      .pipe(gulp.dest(dest + "css"))
+  );
+}
+
+function watch() {
+  gulp.watch(dest + "js/**/*.js", js).on("change", browserSync.reload);
+  gulp.watch(source + "sass/*.scss", styles).on("change", browserSync.reload);
+  gulp.watch(dest + "*.html", html).on("change", browserSync.reload);
+}
+
+function server() {
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: dest,
+    },
+  });
+
+  gulp
+    .watch(source + "sass/**/*.scss", styles)
+    .on("change", browserSync.reload);
+  gulp.watch(dest + "js/**/*.js", js).on("change", browserSync.reload);
+  gulp.watch(dest + "*.html", html).on("change", browserSync.reload);
+}
+
+var build = gulp.series(gulp.parallel(js, styles, html), server, watch);
+
+gulp.task("default", build);
