160ab7612ad33c5d85dff6c7220be5ac4ec0a1fd
asdsadv <62586440+asdsadv@users.noreply.github.com>
2020-04-26 12:24:53 +1000

    Updating file structure

--- builds/ehealthcare/Program.cs
+++ builds/ehealthcare/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace EHealthCare
+namespace Healthcare
 {
     public class Program
     {
--- builds/ehealthcare/Startup.cs
+++ builds/ehealthcare/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace EHealthCare
+namespace Healthcare
 {
     public class Startup
     {
