f9dc7198d5b149df15262e53c3c8dac6a869a4f6
asdsadv <62586440+asdsadv@users.noreply.github.com>
2020-04-26 13:53:58 +1000

    VS studio project that should run with current file structure

--- builds/ehealthcare/Pages/Privacy.cshtml.cs
+++ builds/ehealthcare/Pages/Privacy.cshtml.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-namespace EHealthCare.Pages
+namespace Healthcare.Pages
 {
     public class PrivacyModel : PageModel
     {
